510b9696893121facb1505d7d8009605d9e77616
Amit Saha <amit.saha@outlook.com>
2021-04-18 14:12:00 -0700

    Update sql_queries.py

--- Postgres/sql_queries.py
+++ Postgres/sql_queries.py
@@ -10,10 +10,10 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays(
-songplay_id INTEGER PRIMARY KEY,
-start_time TIMESTAMP,
-user_id INTEGER,
-level VARCHAR,
+songplay_id SERIAL PRIMARY KEY,
+start_time TIMESTAMP NOT NULL,
+user_id INTEGER NOT NULL,
+level VARCHAR NOT NULL,
 song_id VARCHAR,
 artist_id VARCHAR,
 session_id INTEGER,
@@ -23,25 +23,25 @@ user_agent VARCHAR
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-user_id VARCHAR PRIMARY KEY,
+user_id VARCHAR PRIMARY KEY ,
 first_name VARCHAR,
 last_name VARCHAR,
 gender VARCHAR,
-level VARCHAR
+level VARCHAR NOT NULL
 );""")
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
 song_id VARCHAR PRIMARY KEY,
-title VARCHAR,
-artist_id VARCHAR,
+title VARCHAR NOT NULL,
+artist_id VARCHAR NOT NULL,
 year INTEGER,
-duration FLOAT
+duration FLOAT NOT NULL
 );""")
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists(
 artist_id VARCHAR PRIMARY KEY,
-name VARCHAR,
+name VARCHAR NOT NULL,
 location VARCHAR,
 latitude FLOAT,
 longitude FLOAT
@@ -51,19 +51,19 @@ time_table_create = ("""
 
 CREATE TABLE IF NOT EXISTS time(
 start_time TIMESTAMP PRIMARY KEY,
-hour INTEGER,
-day INTEGER,
-week INTEGER,
-month INTEGER,
-year INTEGER,
-weekday INTEGER
+hour INTEGER NOT NULL,
+day INTEGER NOT NULL,
+week INTEGER NOT NULL,
+month INTEGER NOT NULL,
+year INTEGER NOT NULL,
+weekday INTEGER NOT NULL
 );""")
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
 INSERT INTO songplays (songplay_id,start_time, user_id, level, song_id, artist_id, session_id, location , user_agent)
-VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+VALUES (DEFAULT,%s,%s,%s,%s,%s,%s,%s,%s)
 ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
@@ -95,7 +95,7 @@ ON CONFLICT DO NOTHING ;
 
 # FIND SONGS
 song_select = ("""
-SELECT song_id, artists.artist_id
+SELECT songs.song_id, songs.artist_id
 FROM songs JOIN  artists ON songs.artist_id = artists.artist_id
 WHERE songs.title = %s
 AND artists.name = %s
